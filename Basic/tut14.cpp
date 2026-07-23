7d3112f7bc8e3645aad4ec8f038455a5d0f4f18b
DishantPrajapat08 <dishantprajapat08>
2026-07-23 20:29:23 +0530

    New table

--- Basic/tut14.cpp
+++ Basic/tut14.cpp
@@ -1,10 +1,28 @@
+// # include <iostream>
+// using namespace std;
+
+// int main(){
+//     double moon=12.33;
+//     int newmoon =moon;
+    
+//     cout<<newmoon;
+//     return 0;
+// }
 # include <iostream>
 using namespace std;
 
 int main(){
-    double moon=12.33;
-    int newmoon =moon;
+int num1,num2;
+
+    cout<<"Enter the value of num1"<<endl;
+    cin>>num1;
+
+    cout<<"Enter the value of num2"<<endl;
+    cin>>num2;
+
+    cout<<"The sum is"<<endl;
+    cout<<num1*num2;
+
     
-    cout<<newmoon;
     return 0;
 }
---
+++ Basic/tut16.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+
+   int i;
+for(int i=39 ;4>2;i++)
+
+    if(i%39==0){
+        cout<<i<<endl;
+    }
+
+
+    return 0;
+}
